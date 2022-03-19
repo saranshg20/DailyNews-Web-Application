18ebb7f911f92002ef8a690ff881389e851e2396
Saransh Shivhare <saranshg20@iitk.ac.in>
2022-03-19 15:46:42 +0530

    Built the basic structure of the components for the web application

---
+++ news-application/src/components/NewsItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>This is a newsItem component</div>
+    )
+  }
+}
+
+export default NewsItem
