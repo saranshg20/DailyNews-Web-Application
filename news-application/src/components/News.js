3590b72c098c0f6b6b86ada5bfbb9e218615fa1b
Saransh Shivhare <saranshg20@iitk.ac.in>
2022-03-20 21:49:42 +0530

    Adding card components to the web application

--- news-application/src/components/News.js
+++ news-application/src/components/News.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 export class News extends Component {
-  render() {
-    return (
-      <div>
-          This is a news Component
-          <NewsItem/>
-          <NewsItem/>
-          <NewsItem/>
-          <NewsItem/>
-          <NewsItem/>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className='container my-3'>
+                <h3>DailyNews</h3>
+                <div className="row my-3">
+                    <div className="col-md-4">
+                        <NewsItem title={"mytitle"} description={"mydesc"} />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title={"mytitle"} description={"mydesc"} />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title={"mytitle"} description={"mydesc"} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default News
--- news-application/src/components/NewsItem.js
+++ news-application/src/components/NewsItem.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
-  render() {
-    return (
-      <div>This is a newsItem component</div>
-    )
-  }
+    render() {
+        let {title, description} = this.props;
+        return (
+            <div>
+                <div className="card" style={{width: "18rem"}}>
+                    <img src="https://cdn.mlbtraderumors.com/files/2022/03/trevor-story-1024x682.jpg" className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">{title}</h5>
+                            <p className="card-text">{description}</p>
+                            <a href="/newitem" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default NewsItem
