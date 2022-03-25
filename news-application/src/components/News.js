97ebbcd96e520d40cbe71cf58a9caac0e12a565e
Saransh Shivhare <saranshg20@iitk.ac.in>
2022-03-25 19:40:19 +0530

    Fetch the newItem content from newsApi and populated the newsItem in News.js

--- news-application/src/components/News.js
+++ news-application/src/components/News.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 export class News extends Component {
+
+    articles = [];
+        
+
+    constructor(){
+        super();
+        this.state = {
+            articles: this.articles,
+            loading: false
+        }
+    }
+
+    async componentDidMount(){
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=6bd35b34b0d7476ca857f52e5efbf0e3";
+        let data = await fetch(url);
+        let parsedData  = await data.json();
+        this.setState({articles: parsedData.articles})
+        console.log(parsedData);
+    }
+
     render() {
         return (
             <div className='container my-3'>
                 <h3>DailyNews</h3>
                 <div className="row my-3">
-                    <div className="col-md-4">
-                        <NewsItem title={"mytitle"} description={"mydesc"} />
+                    {this.state.articles.map((element)=>{
+                        return <div className="col-md-4">
+                        <NewsItem key={element.url} title={element.title?element.title.slice(0, 45):""} description={element.description?element.description.slice(0, 88):""} imageUrl={element.urlToImage}/>
                     </div>
-                    <div className="col-md-4">
-                        <NewsItem title={"mytitle"} description={"mydesc"} />
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem title={"mytitle"} description={"mydesc"} />
-                    </div>
+                    })}
+                    
                 </div>
             </div>
         )
--- news-application/src/components/NewsItem.js
+++ news-application/src/components/NewsItem.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+    
     render() {
-        let {title, description} = this.props;
+        
+        let {title, description, imageUrl, url} = this.props;
+        let newImageAddress = "http://www.kashilive.com/images/news-162633027160efd49f20a13.jpg";
         return (
-            <div>
+            <div className='my-3'>
                 <div className="card" style={{width: "18rem"}}>
-                    <img src="https://cdn.mlbtraderumors.com/files/2022/03/trevor-story-1024x682.jpg" className="card-img-top" alt="..."/>
+                    <img src={imageUrl===null?newImageAddress:imageUrl} className="card-img-top" alt="..." height={"200px"} width={"100px"}/>
                         <div className="card-body">
-                            <h5 className="card-title">{title}</h5>
-                            <p className="card-text">{description}</p>
-                            <a href="/newitem" className="btn btn-primary">Go somewhere</a>
+                            <h5 className="card-title">{title}...</h5>
+                            <p className="card-text">{description}...</p>
+                            <a href={url} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
                         </div>
                 </div>
             </div>
